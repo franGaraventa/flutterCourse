be1cdbcffe15362cc318826f9a87bee042ffc610
FranGaraventa <garaventafrancisco@gmail.com>
2021-05-09 22:38:41 -0300

    [05/30] Container

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_course/utils/Strings.dart';
+import 'package:flutter_course/utils/dimension.dart';
 
 class MyApp extends StatefulWidget {
   @override
@@ -10,15 +12,35 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "My App",
+      title: appTitle,
       theme: ThemeData(primaryColor: Colors.blueAccent),
       home: Scaffold(
         appBar: AppBar(
-          title: Text("My app"),
+          title: Text(appBarTitle),
         ),
         body: Center(
-          child: Text("Hello World!"),
-        ),
+            child: Container(
+          height: Dimension.containerHeight,
+          width: Dimension.containerWidth,
+          margin: EdgeInsets.all(Dimension.containerMargin),
+          padding: EdgeInsets.all(Dimension.containerPadding),
+          decoration: BoxDecoration(
+              border: Border.all(
+                  width: Dimension.containerBoxDecorationBorder,
+                  color: Colors.black),
+              color: Colors.blueAccent.shade200,
+              borderRadius: BorderRadius.circular(
+                  Dimension.containerBoxDecorationBorderRadius),
+              boxShadow: [
+                BoxShadow(
+                    color: Colors.blueGrey,
+                    offset: Offset(
+                        Dimension.containerBoxDecorationBoxShadowOffset,
+                        Dimension.containerBoxDecorationBoxShadowOffset))
+              ]),
+          child: Text("Text into Container"),
+          transform: Matrix4.rotationZ(Dimension.containerTransformRotationZ),
+        )),
       ),
     );
   }
---
+++ lib/utils/Strings.dart
@@ -0,0 +1,2 @@
+const String appTitle = "My App";
+const String appBarTitle = "App Bar";
